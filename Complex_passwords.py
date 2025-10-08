3522d019b13b2530c83fa33e7be77fec38b8ba5a
JustWain <goshagoog@gmail.com>
2025-10-08 22:21:10 +0500

    Фиксы и добавление документации

--- Complex_passwords.py
+++ Complex_passwords.py
@@ -1,51 +1,53 @@
-from random import choices, shuffle, randint
+from random import choices, shuffle, randint, choice
 from string import ascii_uppercase, ascii_lowercase
 
-def distribution(settings, length):
+def distribution(b_settings, length):
     """
-    Неравномерно распределяет количество символов в каждом виде настроек.
+    Случайно и неравномерно распределяет количество символов в каждом виде настроек.
 
     Args:
-        settings (list): список булевых значений настроек пароля
+        b_settings (list): список булевых значений настроек пароля
         length (int): длина пароля
 
     Returns:
-        
+        Список из количества символов в каждом виде настроек, длина которого зависит от количества выбранных настроек
+        пользователем.
 
     Raises:
-
+        TypeError: если settings не является итерируемым объектом
+        ValueError: если length меньше 3
 
     Example:
-        >
+        > distribution([0,1,0,1], 10)
+        [7,3]
     """
-    number_of_conditions = sum(settings)
-    n = number_of_conditions
-    res = [0]*number_of_conditions
-    for i in range(number_of_conditions):
-        dif = length - sum(res) - n
-        res[i] = randint(1, dif)
-        n -= 1
+    number_of_conditions = sum(b_settings)
+    res = b_settings.copy()
+    for i in range(4):
+        if res[i] == 1:
+            dif = length - sum(res) - number_of_conditions
+            res[i] = randint(1, dif)
+            number_of_conditions -= 1
     dif = length - sum(res)
     if dif == 0: return res
-    res[randint(0, number_of_conditions-1)] += dif
+    res[choice([i for i in range(4) if b_settings[i] == 1])] += dif
     return res
 
 
 def generate_password(settings="", length=0):
     """
-
+    Генерирует пароль исходя из заданных пользователем условий и длины пароля.
 
     Args:
-
+        settings (str): набор условий
+        length (int): длина пароля
 
     Returns:
-
-
-    Raises:
-
+        Пароль (str)
 
     Example:
-        >
+        > generate_password("1234", 8)
+        ,$tM29Ut
     """
     special = "!#$%&()*+,-./:;<=>?@[]^_`{|}~"
     nums = "0123456789"
@@ -62,7 +64,7 @@ def generate_password(settings="", length=0):
                 choices(special, k = quantity_distribution[2]) +
                 choices(nums, k = quantity_distribution[3]))
     shuffle(password)
-    return password
+    return "".join(password)
 
 print("""Настройки пароля:
 1. Наличие нижнего регистра
@@ -72,5 +74,5 @@ print("""Настройки пароля:
 user_settings = input("Выберите настройки пароля (в любом порядке без пробелов): ")
 user_password_length = int(input("Введите длину пароля: "))
 
-print("\nРезультат:", "".join(generate_password(user_settings, user_password_length)), sep="\n")
+print("\nРезультат:", generate_password(user_settings, user_password_length), sep="\n")
 input("\nНажмите Enter для выхода.")
